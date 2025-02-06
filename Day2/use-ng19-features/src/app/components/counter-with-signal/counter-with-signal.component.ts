36fdb7e159bf090d8abde121aea054f312b7731d
Shimon Dahan <shimond@any-techs.co.il>
2025-02-06 11:27:04 +0200

    Enhance counter component to display doubled value and utilize computed properties - STAGE 1

--- Day2/use-ng19-features/src/app/components/counter-with-signal/counter-with-signal.component.ts
+++ Day2/use-ng19-features/src/app/components/counter-with-signal/counter-with-signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CounterService } from '../../services/counter.service';
 import { Observable, of } from 'rxjs';
 
@@ -13,14 +13,16 @@ export class CounterWithSignalComponent {
   count$: Observable<number> = of(20);
   counterService = inject(CounterService);
   count = this.counterService.counterChange;
+  countMul2 = computed(()=> this.count() * 2);
 
   increment() {
     this.counterService.increment();
+    console.log('Current value = ' + this.count());
   }
 
   decrement() {
     this.counterService.decrement();
-    
+
 
   }
 
